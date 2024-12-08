33f9e2753a10560563b4398e614307f17fd34436
HouzuoGuo <guohouzuo@gmail.com>
2024-12-08 17:12:04 +0000

    new directory structure

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -24,26 +24,26 @@
 
 #include "pubkey.h"
 
-static const char* LOG_TAG = "hzgl-air-bridge";
+static const char *LOG_TAG = "hzgl-air-bridge";
 
 /** Callback function for BT events */
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
 
 /** Random device address */
-static esp_bd_addr_t rnd_addr = { 0xFF, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF };
+static esp_bd_addr_t rnd_addr = {0xFF, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
 
 /** Advertisement payload */
 static uint8_t adv_data[31] = {
-	0x1e, /* Length (30) */
-	0xff, /* Manufacturer Specific Data (type 0xff) */
-	0x4c, 0x00, /* Company ID (Apple) */
-	0x12, 0x19, /* Offline Finding type and length */
-	0x00, /* State */
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, /* First two bits */
-	0x00, /* Hint (0x00) */
+    0x1e,       /* Length (30) */
+    0xff,       /* Manufacturer Specific Data (type 0xff) */
+    0x4c, 0x00, /* Company ID (Apple) */
+    0x12, 0x19, /* Offline Finding type and length */
+    0x00,       /* State */
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, /* First two bits */
+    0x00, /* Hint (0x00) */
 };
 
 /* https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/bluetooth/esp_gap_ble.html#_CPPv420esp_ble_adv_params_t */
@@ -72,63 +72,70 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *par
 {
     esp_err_t err;
 
-    switch (event) {
-        case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:
-            esp_ble_gap_start_advertising(&ble_adv_params);
-            break;
+    switch (event)
+    {
+    case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:
+        esp_ble_gap_start_advertising(&ble_adv_params);
+        break;
 
-        case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
-            //adv start complete event to indicate adv start successfully or failed
-            if ((err = param->adv_start_cmpl.status) != ESP_BT_STATUS_SUCCESS) {
-                ESP_LOGE(LOG_TAG, "advertising start failed: %s", esp_err_to_name(err));
-            } else {
-                ESP_LOGI(LOG_TAG, "advertising has started.");
-            }
-            break;
+    case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+        // adv start complete event to indicate adv start successfully or failed
+        if ((err = param->adv_start_cmpl.status) != ESP_BT_STATUS_SUCCESS)
+        {
+            ESP_LOGE(LOG_TAG, "advertising start failed: %s", esp_err_to_name(err));
+        }
+        else
+        {
+            ESP_LOGI(LOG_TAG, "advertising has started.");
+        }
+        break;
 
-        case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
-            if ((err = param->adv_stop_cmpl.status) != ESP_BT_STATUS_SUCCESS){
-                ESP_LOGE(LOG_TAG, "adv stop failed: %s", esp_err_to_name(err));
-            }
-            else {
-                ESP_LOGI(LOG_TAG, "stop adv successfully");
-            }
-            break;
-        default:
-            break;
+    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+        if ((err = param->adv_stop_cmpl.status) != ESP_BT_STATUS_SUCCESS)
+        {
+            ESP_LOGE(LOG_TAG, "adv stop failed: %s", esp_err_to_name(err));
+        }
+        else
+        {
+            ESP_LOGI(LOG_TAG, "stop adv successfully");
+        }
+        break;
+    default:
+        break;
     }
 }
 
-void set_addr_from_key(esp_bd_addr_t addr, uint8_t *public_key) {
-	addr[0] = public_key[0] | 0b11000000;
-	addr[1] = public_key[1];
-	addr[2] = public_key[2];
-	addr[3] = public_key[3];
-	addr[4] = public_key[4];
-	addr[5] = public_key[5];
+void set_addr_from_key(esp_bd_addr_t addr, uint8_t *public_key)
+{
+    addr[0] = public_key[0] | 0b11000000;
+    addr[1] = public_key[1];
+    addr[2] = public_key[2];
+    addr[3] = public_key[3];
+    addr[4] = public_key[4];
+    addr[5] = public_key[5];
 }
 
-void set_payload_from_key(uint8_t *payload, uint8_t *public_key) {
+void set_payload_from_key(uint8_t *payload, uint8_t *public_key)
+{
     /* copy last 22 bytes */
-	memcpy(&payload[7], &public_key[6], 22);
-	/* append two bits of public key */
-	payload[29] = public_key[0] >> 6;
+    memcpy(&payload[7], &public_key[6], 22);
+    /* append two bits of public key */
+    payload[29] = public_key[0] >> 6;
 }
 
 void setup(void)
 {
     Serial.begin(115200);
-    Serial.println("starting up");
+    ESP_LOGI(LOG_TAG, "starting up");
     vTaskDelay(pdMS_TO_TICKS(5000));
-    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     esp_bt_controller_init(&bt_cfg);
     esp_bt_controller_enable(ESP_BT_MODE_BLE);
-    Serial.println("bluetooth initialised");
+    ESP_LOGI(LOG_TAG, "bluetooth initialised");
 
     esp_bluedroid_init();
     esp_bluedroid_enable();
-    Serial.println("bluedroid initialised");
+    ESP_LOGI(LOG_TAG, "bluedroid initialised");
 
     set_addr_from_key(rnd_addr, public_key);
     set_payload_from_key(adv_data, public_key);
@@ -136,16 +143,19 @@ void setup(void)
     ESP_LOGI(LOG_TAG, "using device address: %02x %02x %02x %02x %02x %02x", rnd_addr[0], rnd_addr[1], rnd_addr[2], rnd_addr[3], rnd_addr[4], rnd_addr[5]);
 
     esp_err_t status;
-    if ((status = esp_ble_gap_register_callback(esp_gap_cb)) != ESP_OK) {
+    if ((status = esp_ble_gap_register_callback(esp_gap_cb)) != ESP_OK)
+    {
         ESP_LOGE(LOG_TAG, "gap register error: %s", esp_err_to_name(status));
         return;
     }
 
-    if ((status = esp_ble_gap_set_rand_addr(rnd_addr)) != ESP_OK) {
+    if ((status = esp_ble_gap_set_rand_addr(rnd_addr)) != ESP_OK)
+    {
         ESP_LOGE(LOG_TAG, "couldn't set random address: %s", esp_err_to_name(status));
         return;
     }
-    if ((esp_ble_gap_config_adv_data_raw((uint8_t*)&adv_data, sizeof(adv_data))) != ESP_OK) {
+    if ((esp_ble_gap_config_adv_data_raw((uint8_t *)&adv_data, sizeof(adv_data))) != ESP_OK)
+    {
         ESP_LOGE(LOG_TAG, "couldn't configure BLE adv: %s", esp_err_to_name(status));
         return;
     }
@@ -154,6 +164,6 @@ void setup(void)
 
 void loop()
 {
-    Serial.println("still alive");
+    ESP_LOGI(LOG_TAG, "still alive");
     vTaskDelay(pdMS_TO_TICKS(3000));
 }
